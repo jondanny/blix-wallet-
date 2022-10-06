bb063d46a20ca5cb1a37451d202a14b2393a8ef9
jondanny <jdnguyen928@gmail.com>
2022-10-06 18:38:42 +0300

    Added user tests

--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -9,6 +9,7 @@ import { TestHelper } from '@test/helpers/test.helper';
 import { UserStatus } from '@src/user/user.types';
 import { faker } from '@faker-js/faker';
 import { User } from '@src/user/user.entity';
+import { TicketProviderUserIdentifier } from '@src/ticket-provider/ticket-provider.types';
 
 describe('User (e2e)', () => {
   let app: INestApplication;
@@ -60,6 +61,90 @@ describe('User (e2e)', () => {
           ]),
         );
         expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+      });
+  });
+
+  it('should check user duplicates by email', async () => {
+    const ticketProvider = await TicketProviderFactory.create({ userIdentifier: TicketProviderUserIdentifier.Email });
+    const token = await testHelper.createTicketProviderToken(ticketProvider.id);
+    const email = faker.internet.email();
+    await UserFactory.create({ ticketProviderId: ticketProvider.id, email });
+
+    await request(app.getHttpServer())
+      .post('/api/v1/users')
+      .send({
+        email,
+        name: faker.name.firstName(),
+        phoneNumber: faker.phone.number('+38050#######').toString(),
+      })
+      .set('Accept', 'application/json')
+      .set('api-token', token)
+      .then((response) => {
+        expect(response.body.message).toEqual(
+          expect.arrayContaining([`User with identifier '${email}' already exists`]),
+        );
+        expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+      });
+  });
+
+  it('should check user duplicates by phone number', async () => {
+    const ticketProvider = await TicketProviderFactory.create({
+      userIdentifier: TicketProviderUserIdentifier.PhoneNumber,
+    });
+    const token = await testHelper.createTicketProviderToken(ticketProvider.id);
+    const phoneNumber = faker.phone.number('+38050#######').toString();
+    await UserFactory.create({ ticketProviderId: ticketProvider.id, phoneNumber });
+
+    await request(app.getHttpServer())
+      .post('/api/v1/users')
+      .send({
+        email: faker.internet.email(),
+        name: faker.name.firstName(),
+        phoneNumber,
+      })
+      .set('Accept', 'application/json')
+      .set('api-token', token)
+      .then((response) => {
+        expect(response.body.message).toEqual(
+          expect.arrayContaining([`User with identifier '${phoneNumber}' already exists`]),
+        );
+        expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+      });
+  });
+
+  it('should allow to create user if it exists in another ticket provider', async () => {
+    const ticketProvider = await TicketProviderFactory.create();
+    const ticketProvider2 = await TicketProviderFactory.create();
+    const token = await testHelper.createTicketProviderToken(ticketProvider.id);
+
+    const userData = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number('+38050#######').toString(),
+    };
+
+    await UserFactory.create({ ticketProviderId: ticketProvider2.id, ...userData });
+
+    await request(app.getHttpServer())
+      .post('/api/v1/users')
+      .send(userData)
+      .set('Accept', 'application/json')
+      .set('api-token', token)
+      .then(async (response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            ...userData,
+            uuid: expect.any(String),
+            status: UserStatus.Creating,
+          }),
+        );
+        expect(response.status).toBe(HttpStatus.CREATED);
+
+        const newUser = await AppDataSource.manager
+          .getRepository(User)
+          .findOne({ where: { uuid: response.body.uuid } });
+
+        expect(newUser.ticketProviderId).toEqual(ticketProvider.id);
       });
   });
 
