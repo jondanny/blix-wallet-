7f5488bd118b439d9714e68d74481e2534c77692
jondanny <jdnguyen928@gmail.com>
2022-11-22 13:59:57 +0200

    Remove file

--- create-kafka-topics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-topics=("healthcheck" "user.create" "user.create.reply" "ticket.create" "ticket.create.reply" "ticket.transfer" "ticket.transfer.reply" "ticket.delete" "ticket.delete.reply", "message.send", "message.send.reply")
-
-for i in "${topics[@]}"
-do
-  echo "Creating topic $i..."
-  docker exec -it validate-kafka bash -c "./bin/kafka-topics.sh --create --topic $i --partitions 1 --replication-factor 1 --bootstrap-server localhost:9093"
-done
