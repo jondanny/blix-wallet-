0748761399e05951058322629da1ea2c1a73ef1d
jondanny <jdnguyen928@gmail.com>
2022-12-19 10:27:40 +0200

    Fix typo

--- src/ticket/messages/ticket-validate.message.ts
+++ src/ticket/messages/ticket-validate.message.ts
@@ -1,13 +1,9 @@
-import { EncryptedData } from '@src/ticket-provider-encryption-key/ticket-provider-encryption.types';
-import { User } from '@src/user/user.entity';
 import { v4 as uuid } from 'uuid';
 import { Ticket } from '../ticket.entity';
 
 export class TicketValidateMessage {
   operationUuid: string;
   ticket: Ticket;
-  user: User;
-  encryptedData?: EncryptedData;
 
   constructor(data: Partial<TicketValidateMessage>) {
     Object.assign(this, data);
