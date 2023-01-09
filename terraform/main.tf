a51fe236e88e7818e71fc219164902ade13f8e62
jondanny <jdnguyen928@gmail.com>
2023-01-09 10:29:06 +0200

    Remove duplicate producer

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,16 +55,3 @@ module "api_gateway_consumer_ecs" {
   api_gateway_erc_url = module.ecr_repository.api_gateway_erc_url
   secret_manager_id = module.secrets_manager.aws_secret_manager_id
 }
-
-module "api_gateway_producer_ecs" {
-  source = "./modules/aws_ecs_producer"
-
-  depends_on = [module.ecr_repository, module.ecs_agent, module.networking, module.secrets_manager]
-
-  instance_security_group_id = module.networking.instance_sg_id
-  private_subnet_a_id = module.networking.private_subnet_a_id
-  private_subnet_b_id = module.networking.private_subnet_b_id
-  ecs_agent_name = module.ecs_agent.ecs_agent_name
-  api_gateway_erc_url = module.ecr_repository.api_gateway_erc_url
-  secret_manager_id = module.secrets_manager.aws_secret_manager_id
-}
